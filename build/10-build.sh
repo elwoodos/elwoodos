546764c1b6c8f510c141997bcd0ecbce9351bc8e
elwoodos <elwood@bosh.org>
2026-01-19 18:18:12 +0000

    Update 10-build.sh dnf5 install qalc

--- build/10-build.sh
+++ build/10-build.sh
@@ -45,6 +45,7 @@ echo "::group:: Install Packages"
 
 # Install packages using dnf5
 # Example: dnf5 install -y tmux
+dnf5 install -y qalc
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
